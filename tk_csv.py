62bfa2a105b0445aa540eee36780aea9fb9b663b
Junnewbe <85324653+Junnewbe@users.noreply.github.com>
2021-06-14 13:05:44 +0900

    Update tk_csv.py

--- tk_csv.py
+++ tk_csv.py
@@ -6,6 +6,7 @@ import os #폴더 만들고 삭제
 print(os.getcwd())#현재 디렉토리 위치 알려줌
 
 
+
 data = pd.read_csv("seoul.csv", encoding="cp949")
 
 #.현재폴더 ..상위폴더
@@ -26,19 +27,21 @@ def click():
     oup.insert(END, def_word)
 
 window = Tk()
-window.title("My Dictionary")
+window.title("서울시 실외운동기구 현황")
+window.geometry("640x400+100+100") #출력창 크기& 위치(+x값,+y값)
+window.resizable(False,False) #창 늘리기 상하,좌우 불가!
 
 #입력상자 설명 레이블
-label = Label(window, text="원하는 단어 입력후, 엔터 키 누르기 ") #Label은 라벨링 텍스트를 넣을수 있는공간
+label = Label(window, text="기구 종류(실외 운동시설 이름입력!) ") #Label은 라벨링 텍스트를 넣을수 있는공간
 label.grid(row=0, column=0, sticky=W) #W는 west다 고정시킬 위치 grid는 위치에 장착??
 
 # 02 텍스트 입력이 가능한 상자(Entry)
-entry = Entry(window, width=15, bg="light green") #entry는 입력을 넣을수 있는 상자
+entry = Entry(window, width=30, bg="light green") #entry는 입력을 넣을수 있는 상자
 entry.grid(row=1, column=0, sticky=W)
 
 # 03제출 버튼 추가
-btn = Button(window, text="제출", width=5, command=click)
-btn.grid (row=2, column=0, sticky=E)
+btn = Button(window, text="제출", width=5, command=click, borderwidth=3)
+btn.grid(row=1, column=1, sticky=W)
 
 # 04 설명 레이블-의미
 min=Label(window, text="\n보유기구:")
@@ -47,7 +50,7 @@ min.grid(row=3, column=0, sticky= W)
 
 # 05 텍스트 박스 입력 상자
 #columnspan=2 (4,0)~,(4,1)위치까지 분포
-oup = Text(window, width = 50, height = 6, wrap = WORD, background="light green")
+oup = Text(window, width = 100, height = 50, wrap = WORD, background="light green")
 oup.grid(row=4, column=0, columnspan=2, sticky= W)
 
 window.mainloop()
